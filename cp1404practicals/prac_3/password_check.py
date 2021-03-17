031233ee98527f507441ea246c9407f77b8075af
Lee <lee.crawford@my.jcu.edu.au>
2021-03-17 11:31:36 +1000

    Refactor password check program to use functions

--- cp1404practicals/prac_3/password_check.py
+++ cp1404practicals/prac_3/password_check.py
@@ -6,9 +6,22 @@ password_check
 """
 
 
-minimum_length = 4
-password = input("please enter your password: ")
-while len(password) < minimum_length:
-    print(f"Password must be at least {minimum_length} character/s long")
+def main():
+    minimum_length = 4
+    password = get_password(minimum_length)
+    print_number_of_astericks(password)
+
+
+def print_number_of_astericks(password):
+    print("*" * len(password))
+
+
+def get_password(minimum_length):
     password = input("please enter your password: ")
-print("*" * len(password))
+    while len(password) < minimum_length:
+        print(f"Password must be at least {minimum_length} character/s long")
+        password = input("please enter your password: ")
+    return password
+
+
+main()

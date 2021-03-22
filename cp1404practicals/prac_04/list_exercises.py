d8fd5dc679fc7aeb8d64becccc886cd3af57dea6
Lee <lee.crawford@my.jcu.edu.au>
2021-03-22 14:42:32 +1000

    Add list exercises program

---
+++ cp1404practicals/prac_04/list_exercises.py
@@ -0,0 +1,43 @@
+"""
+Lee Crawford
+CP1404/CP5632 Practical
+Basic list operations
+"""
+
+INPUT_TOTAL = 5
+
+
+def main():
+    number_count = 0
+    print("Please enter {} numbers".format(INPUT_TOTAL))
+    numbers = get_list_of_numbers(number_count)
+    display_list(numbers)
+    display_interesting_information(numbers)
+
+
+def get_list_of_numbers(number_count):
+    """Get a list of numbers from the user INPUT_TOTAL long"""
+    numbers = []
+    while number_count != INPUT_TOTAL:
+        number = int(input("Number: "))
+        numbers.append(number)
+        number_count += 1
+    return numbers
+
+
+def display_list(numbers):
+    """Print numbers from a list"""
+    for number in numbers:
+        print(number)
+
+
+def display_interesting_information(numbers):
+    """Print interesting information from a list of numbers"""
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers)/INPUT_TOTAL))
+
+
+main()
